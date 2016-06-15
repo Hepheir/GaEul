41282fbc6f42d2789290568103d370184528e9e9
Hepheir <hepheir@gmail.com>
2016-06-15 23:42:36 +0900

    Create korean.js
    for GaEul 1.1

---
+++ korean.js
@@ -0,0 +1,134 @@
+function get_words(i) {
+  i = i.replace(/\+AND/g,"");
+  i = i.replace(/\+DO/g,"");
+  i = i.replace(/;/g,".");
+  i = i.replace(/\+/g," ");
+  var s = new Array();
+  var b = 1;
+  for (var a = 0; i[a] != '.' && i[a] != undefined; a++) {
+    for (s[b] = ''; i[a] != ' ' && i[a] != '.' && i[a] != undefined; a++) {
+      s[b] += i[a];
+    }
+    b++;
+  }
+  s[0] = --b;
+  return s;
+}
+
+//마침표를 기준으로 문장을 분리하여 배열로 저장하여 반환해주는 함수.
+function sort_sentences(i){
+  i = i.replace(/\./g,"+;");
+  i = i.replace(/\,/g,"+;");
+  i = i.replace(/\!/g,"+;");
+  i = i.replace(/\?/g,"+;");
+  i = i.replace(/ /g,"+");
+  i = i.replace(/;\+/g,";");
+  var sentences = new Array();
+  var n = 0;
+  sentences[n] = '';
+  for (var a = 0; a < i.length; a++) {
+    sentences[n] += i[a];
+    if (i[a] == ';' && i[a+1] != undefined) {
+      n++;
+      sentences[n] = '';
+    }
+  }
+  return sentences;
+}
+
+function fix_words(i,debug){
+  //예외처리 input
+  i = i.replace(/의의/g,"ㅇㅇ1");
+
+  //구어체 관련
+  i = i.replace(/어\+;/g,"다+;");
+  i = i.replace(/자\+;/g,"다+;");
+  i = i.replace(/게\+;/g,"다+;");
+  i = i.replace(/죠\+;/g,"다+;");
+  i = i.replace(/거라\+;/g,"다+;");
+  i = i.replace(/할\+;/g,"하다+;");
+
+  //특수처리
+  i = i.replace(/과는\+/g,"+AND+");
+  i = i.replace(/과\+/g,"+AND+");
+  i = i.replace(/와는\+/g,"+AND+");
+  i = i.replace(/와\+/g,"+AND+");
+  i = i.replace(/하고\+/g,"+AND+");
+  i = i.replace(/랑\+/g,"+AND+");
+
+  i = i.replace(/같다\+/g,"+LIKE+");
+  i = i.replace(/같네\+/g,"+LIKE+");
+  i = i.replace(/같은\+/g,"+LIKE+");
+  i = i.replace(/처럼\+/g,"+LIKE+");
+  i = i.replace(/마치\+/g,"+LIKE+");
+  i = i.replace(/좀\+/g,"+");
+
+
+  i = i.replace(/한다\+/g,"+DO+");
+  i = i.replace(/하는\+/g,"+DO+");
+  i = i.replace(/하다\+/g,"+DO+");
+  i = i.replace(/했다\+/g,"+PAST+DO+");
+  i = i.replace(/하였다\+/g,"+PAST+DO+");
+
+  i = i.replace(/하지만\+/g,"+BUT+");
+  i = i.replace(/그러나\+/g,"+BUT+");
+  i = i.replace(/그렇지만\+/g,"+BUT+");
+  i = i.replace(/같지만\+/g,"+LIKE+BUT+");
+
+
+  //조사
+  i = i.replace(/은\+/g,"+");
+  i = i.replace(/는\+/g,"+");
+  i = i.replace(/이\+/g,"+");
+  i = i.replace(/가\+/g,"+");
+
+  i = i.replace(/을\+/g,"+");
+  i = i.replace(/를\+/g,"+");
+  i = i.replace(/도\+/g,"+");
+  i = i.replace(/역시\+/g,"+");
+  i = i.replace(/에서\+/g,"+");
+  i = i.replace(/에\+/g,"+");
+  i = i.replace(/의\+/g,"+");
+
+  //일반
+  i = i.replace(/하세요\+/g,"+");
+  i = i.replace(/세요\+/g,"+");
+  i = i.replace(/하네\+/g,"+");
+  i = i.replace(/하려\+/g,"+");
+  i = i.replace(/시게\+/g,"+");
+  i = i.replace(/이다\+/g,"+");
+  i = i.replace(/였다\+/g,"+PAST+");
+
+  i = i.replace(/해요\+/g,"+");
+  i = i.replace(/해\+/g,"+");
+  i = i.replace(/나요\+/g,"+");
+  i = i.replace(/요\+/g,"+");
+  i = i.replace(/하도록\+/g,"+");
+
+  i = i.replace(/입니다\+/g,"+");
+  i = i.replace(/니다\+/g,"+");
+  i = i.replace(/이다\+/g,"+");
+  i = i.replace(/되다\+/g,"+");
+  i = i.replace(/된다\+/g,"+");
+  i = i.replace(/다\+/g,"+");
+  i = i.replace(/되는지\+/g,"+");
+  i = i.replace(/는지\+/g,"+");
+
+  i = i.replace(/야\+/g,"+");
+
+  i = i.replace(/에게\+/g,"+");
+  i = i.replace(/께\+/g,"+");
+  i = i.replace(/으로\+/g,"+");
+  i = i.replace(/로\+/g,"+");
+
+  //기호 & 디버그
+  i = i.replace(/\+\+/g,"+");
+
+  //예외처리 output
+  i = i.replace(/ㅇㅇ1/g,"의의");
+
+  if (debug == 'on'){
+    alert(i);
+  }
+  return i;
+}
